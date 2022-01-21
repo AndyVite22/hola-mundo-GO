b5125001df944be72ea72964b56421c9f2745016
AndyVite22 <86022812+AndyVite22@users.noreply.github.com>
2022-01-20 22:41:18 -0600

    Add files via upload

---
+++ HolaMundo.go
@@ -0,0 +1,17 @@
+/**
+ * HolaMundo
+ * Descripción: Programa para imprimir texto
+ */
+
+// paquete al que pertenece el programa
+package main
+
+// librerias necesarias para imprimir
+import "fmt"
+
+// la función main empieza la ejecución de la aplicación en Go
+func main() {
+
+	// función para imprimir una cadena en consola
+	fmt.Println("Hola Mundo con programación en Go")
+} // fin de la funcion main
